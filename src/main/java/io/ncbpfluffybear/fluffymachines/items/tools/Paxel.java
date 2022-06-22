23a6b1c4863e483b87f9a33abb778c4128faa1a9
FluffyBear <31554056+ncbpfluffybear@users.noreply.github.com>
2022-06-22 02:54:19 -0500

    Fixed a compatibility issue with the Paxel

--- src/main/java/io/ncbpfluffybear/fluffymachines/items/tools/Paxel.java
+++ src/main/java/io/ncbpfluffybear/fluffymachines/items/tools/Paxel.java
@@ -1,13 +1,18 @@
 package io.ncbpfluffybear.fluffymachines.items.tools;
 
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.attributes.NotPlaceable;
 import io.github.thebusybiscuit.slimefun4.utils.tags.SlimefunTag;
 import io.ncbpfluffybear.fluffymachines.FluffyMachines;
 import io.ncbpfluffybear.fluffymachines.utils.FluffyItems;
-import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
-import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
-import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
-import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Tag;
@@ -15,39 +20,32 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockDamageEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Paxel extends SlimefunItem implements Listener, NotPlaceable {
 
     public final Set<Material> axeBlocks = Stream.of(
-        Tag.LOGS.getValues(),
-        Tag.PLANKS.getValues(),
-        Tag.WOODEN_STAIRS.getValues(),
-        Tag.SIGNS.getValues(),
-        Tag.WOODEN_FENCES.getValues(),
-        Tag.FENCE_GATES.getValues(),
-        Tag.WOODEN_TRAPDOORS.getValues(),
-        Tag.WOODEN_PRESSURE_PLATES.getValues(),
-        Tag.WOODEN_DOORS.getValues(),
-        Tag.WOODEN_SLABS.getValues(),
-        Tag.WOODEN_BUTTONS.getValues(),
-        Tag.BANNERS.getValues(),
-        Tag.LEAVES.getValues(),
-        new HashSet<>(Arrays.asList(Material.CHEST, Material.TRAPPED_CHEST, Material.CRAFTING_TABLE, Material.SMITHING_TABLE,
-            Material.LOOM, Material.CARTOGRAPHY_TABLE, Material.FLETCHING_TABLE, Material.BARREL, Material.JUKEBOX,
-            Material.CAMPFIRE, Material.BOOKSHELF, Material.JACK_O_LANTERN, Material.CARVED_PUMPKIN,
-            Material.PUMPKIN, Material.MELON, Material.COMPOSTER, Material.BEEHIVE, Material.BEE_NEST,
-            Material.NOTE_BLOCK, Material.LADDER, Material.COCOA_BEANS, Material.DAYLIGHT_DETECTOR, Material.MUSHROOM_STEM,
-            Material.RED_MUSHROOM_BLOCK, Material.RED_MUSHROOM_BLOCK, Material.BAMBOO, Material.VINE, Material.LECTERN))
+            Tag.LOGS.getValues(),
+            Tag.PLANKS.getValues(),
+            Tag.WOODEN_STAIRS.getValues(),
+            Tag.SIGNS.getValues(),
+            Tag.WOODEN_FENCES.getValues(),
+            Tag.FENCE_GATES.getValues(),
+            Tag.WOODEN_TRAPDOORS.getValues(),
+            Tag.WOODEN_PRESSURE_PLATES.getValues(),
+            Tag.WOODEN_DOORS.getValues(),
+            Tag.WOODEN_SLABS.getValues(),
+            Tag.WOODEN_BUTTONS.getValues(),
+            Tag.BANNERS.getValues(),
+            Tag.LEAVES.getValues(),
+            new HashSet<>(Arrays.asList(Material.CHEST, Material.TRAPPED_CHEST, Material.CRAFTING_TABLE, Material.SMITHING_TABLE,
+                    Material.LOOM, Material.CARTOGRAPHY_TABLE, Material.FLETCHING_TABLE, Material.BARREL, Material.JUKEBOX,
+                    Material.CAMPFIRE, Material.BOOKSHELF, Material.JACK_O_LANTERN, Material.CARVED_PUMPKIN,
+                    Material.PUMPKIN, Material.MELON, Material.COMPOSTER, Material.BEEHIVE, Material.BEE_NEST,
+                    Material.NOTE_BLOCK, Material.LADDER, Material.COCOA_BEANS, Material.DAYLIGHT_DETECTOR, Material.MUSHROOM_STEM,
+                    Material.RED_MUSHROOM_BLOCK, Material.RED_MUSHROOM_BLOCK, Material.BAMBOO, Material.VINE, Material.LECTERN))
     ).flatMap(Set::stream).collect(Collectors.toSet());
 
     public Paxel(ItemGroup category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
@@ -57,28 +55,20 @@ public class Paxel extends SlimefunItem implements Listener, NotPlaceable {
     }
 
     @EventHandler(ignoreCancelled = true)
-    private void onMine(PlayerInteractEvent e) {
-        if (e.getAction() != Action.LEFT_CLICK_BLOCK) {
-            return;
-        }
-
+    private void onMine(BlockDamageEvent e) {
         Player p = e.getPlayer();
         SlimefunItem sfItem = SlimefunItem.getByItem(p.getInventory().getItemInMainHand());
 
         if (sfItem != null && sfItem == FluffyItems.PAXEL.getItem()) {
             boolean netherite = false;
-            Block b = e.getClickedBlock();
+            Block b = e.getBlock();
             ItemStack item = p.getInventory().getItemInMainHand();
-
-            if (b == null) {
-                return;
-            }
 
             Material blockType = b.getType();
 
             if (item.getType() == Material.NETHERITE_PICKAXE
-                || item.getType() == Material.NETHERITE_AXE
-                || item.getType() == Material.NETHERITE_SHOVEL
+                    || item.getType() == Material.NETHERITE_AXE
+                    || item.getType() == Material.NETHERITE_SHOVEL
             ) {
                 netherite = true;
             }
