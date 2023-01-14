e781c86d78fccd33b8e53e8143a8341058d0089d
NCBPFluffyBear <31554056+ncbpfluffybear@users.noreply.github.com>
2023-01-14 15:12:57 -0600

    Fixes #132

--- src/main/java/io/ncbpfluffybear/fluffymachines/multiblocks/CrankGenerator.java
+++ src/main/java/io/ncbpfluffybear/fluffymachines/multiblocks/CrankGenerator.java
@@ -1,13 +1,16 @@
 package io.ncbpfluffybear.fluffymachines.multiblocks;
 
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetProvider;
 import io.github.thebusybiscuit.slimefun4.core.multiblocks.MultiBlockMachine;
+import io.ncbpfluffybear.fluffymachines.multiblocks.components.GeneratorCore;
 import io.ncbpfluffybear.fluffymachines.utils.Utils;
 import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.ncbpfluffybear.fluffymachines.utils.FluffyItems;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -18,7 +21,7 @@ import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
 
-public class CrankGenerator extends MultiBlockMachine implements EnergyNetProvider {
+public class CrankGenerator extends MultiBlockMachine {
 
     public static final int RATE = 16;
     public static final int CAPACITY = 64;
@@ -28,24 +31,13 @@ public class CrankGenerator extends MultiBlockMachine implements EnergyNetProvid
             FluffyItems.GENERATOR_CORE, null}, BlockFace.SELF);
     }
 
-    @Override
-    public int getGeneratedOutput(@Nonnull Location location, @Nonnull Config config) {
-        return 0;
-    }
-
-    @Override
-    public int getCapacity() {
-        return CAPACITY;
-    }
-
     public void onInteract(Player p, Block b) {
-        String id;
-        Block core = b.getRelative(BlockFace.DOWN);
-        if (BlockStorage.hasBlockInfo(core)) {
-            id = BlockStorage.getLocationInfo(core.getLocation(), "id");
+        Block coreBlock = b.getRelative(BlockFace.DOWN);
+        if (BlockStorage.hasBlockInfo(coreBlock)) {
+            SlimefunItem core = BlockStorage.check(coreBlock.getLocation());
 
-            if (id.equals("GENERATOR_CORE")) {
-                addCharge(core.getLocation(), RATE);
+            if (core instanceof GeneratorCore) {
+                ((GeneratorCore) core).addCharge(coreBlock.getLocation(), RATE);
                 p.playSound(p.getLocation(), Sound.BLOCK_PISTON_EXTEND, 0.5F, 0.5F);
 
             } else {
--- src/main/java/io/ncbpfluffybear/fluffymachines/multiblocks/components/GeneratorCore.java
+++ src/main/java/io/ncbpfluffybear/fluffymachines/multiblocks/components/GeneratorCore.java
@@ -1,16 +1,18 @@
 package io.ncbpfluffybear.fluffymachines.multiblocks.components;
 
-import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetComponent;
-import io.github.thebusybiscuit.slimefun4.core.networks.energy.EnergyNetComponentType;
-import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetProvider;
+import io.github.thebusybiscuit.slimefun4.core.networks.energy.EnergyNetComponentType;
+import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
+import org.bukkit.Location;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
 
-public class GeneratorCore extends SlimefunItem implements EnergyNetComponent {
+public class GeneratorCore extends SlimefunItem implements EnergyNetProvider {
 
     public GeneratorCore(ItemGroup category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(category, item, recipeType, recipe);
@@ -20,6 +22,13 @@ public class GeneratorCore extends SlimefunItem implements EnergyNetComponent {
     @Override
     public EnergyNetComponentType getEnergyComponentType() {
         return EnergyNetComponentType.GENERATOR;
+    }
+
+    // Making this block an EnergyNetProvider instead of an EnergyNetComponent causes it to tick. However, this block will not actively
+    // push out energy unless made a generator.
+    @Override
+    public int getGeneratedOutput(@Nonnull Location l, @Nonnull Config data) {
+        return 0;
     }
 
     @Override
