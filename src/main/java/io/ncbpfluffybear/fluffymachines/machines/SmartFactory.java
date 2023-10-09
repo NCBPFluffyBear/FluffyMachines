4740a3a9fc8a4dca3c35366296076a32e7a125eb
ybw0014 <i@guizhanss.com>
2023-10-09 19:26:06 -0400

    chore(SmartFactory): suggestion from FluffyBear

--- src/main/java/io/ncbpfluffybear/fluffymachines/machines/SmartFactory.java
+++ src/main/java/io/ncbpfluffybear/fluffymachines/machines/SmartFactory.java
@@ -94,7 +94,8 @@ public class SmartFactory extends SlimefunItem implements EnergyNetComponent, Re
                         "&eThis row is reserved for coal for cargo."
                 ));
                 this.addItem(18, new CustomItemStack(Material.YELLOW_STAINED_GLASS_PANE, "&bMisc Slots",
-                        "&eThe remaining rows accept any item.", "&eCargo will only fill stacks.",
+                        "&eThe remaining rows accept any item.", "&eCargo will fill stacks after",
+                        "&eat least one of each recipe", "&erequirement has been inserted.",
                         "&eNeed to keep one more item in", "&eeach stack as template."
                 ));
                 this.addMenuClickHandler(RECIPE_SLOT, ChestMenuUtils.getEmptyClickHandler());
